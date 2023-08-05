121037289001019159b72a190deb5291add94d74
Thomas Arend <thomas@arend-rhb.de>
2023-08-05 19:16:30 +0200

    Stationlist as first table

--- html/index.php
+++ html/index.php
@@ -19,7 +19,7 @@ include_once 'solarlib.php';
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="expires" content="0" /> 
-	<meta http-equiv="refresh" content="300" />
+	<meta http-equiv="refresh" content="60" />
 	<meta http-equiv="cache-control" content="no-cache" />
 	<meta http-equiv="pragma" content="no-cache" />
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
@@ -33,16 +33,16 @@ include_once 'solarlib.php';
     <h1>Solarkraftwerk</h1>
 </div>
 
+<div id="tables" >
 <?php
 
-    SolarReport();
     StationList();
+    SolarReport();
 
-    $mysqli->close();
-
-    
+    $mysqli->close();    
     
 ?>
+</div>
 
 <main class="container">
 
